4d09ea097cfab1eb14d567416f8e29a09c0ad634
kelvinsekx <utdkelvin@gmail.com>
2025-12-07 03:11:16 +0100

    modify auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,29 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, ConflictException, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './auth.dto';
 import { AuthService } from './auth.service';
+import { SupabaseService } from 'src/supabase/supabase.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private supabaseService: SupabaseService,
+  ) {}
   /**
    */
   // auth/signup
   @Post('signup')
-  signup(@Body() body: CreateUserDto) {
-    return this.authService.create(body);
+  async signup(@Body() body: CreateUserDto) {
+    const { data } = await this.supabaseService
+      .getClient()
+      .from('users')
+      .select('*')
+      .eq('email', body.email);
+
+    if (data?.length) {
+      throw new ConflictException('User with this email already exists.');
+    }
+
+    return await this.authService.create(body);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import bcrypt from 'bcrypt';
+import { SupabaseService } from 'src/supabase/supabase.service';
+import { CreateUserDto } from './auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(body) {
-    return body;
+  constructor(private supabaseService: SupabaseService) {}
+
+  async create(body: CreateUserDto) {
+    const saltRounds = 14;
+    const hashedPassword = await bcrypt.hash(body.password, saltRounds);
+
+    const { error } = await this.supabaseService
+      .getClient()
+      .from('users')
+      .insert({
+        email: body.email,
+        password: hashedPassword,
+      });
+
+    if (error) {
+      console.error('Supabase insertion error:', error);
+      throw new InternalServerErrorException(
+        'Failed to create user in the database.',
+      );
+    }
+
+    return {
+      message: 'User created successfully',
+      email: body.email,
+    };
   }
 }
