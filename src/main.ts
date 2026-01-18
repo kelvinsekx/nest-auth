534851883b6d0020e0a928925a0c9ae93ac19230
kelvinsekx <utdkelvin@gmail.com>
2026-01-18 11:18:40 +0100

    enable api doc with swagger

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Playground APIs')
+    .setDescription('The Movies API description')
+    .setVersion('1.0')
+    .addTag('movies')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
